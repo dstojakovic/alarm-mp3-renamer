e13b1bfdb1caf484336503e0484f6a89454672e2
dako <dako@local>
2017-04-07 17:22:30 +0200

    Ispravke za novi format imena fajla.
    Signed-off-by: dako <dako@local>

--- main.py
+++ main.py
@@ -5,22 +5,39 @@ def main():
   # TODO: GUI
   
   import os
+  import sys
   import pdb
   import logging
   
   # variables setup; TODO config
   logging.basicConfig(level=logging.INFO)
   logger = logging.getLogger(__name__)
+  logger.info(sys.prefix)
+  logger.debug('Config started.')
+  configFile = 'config.xml'
   directoryMaster = 'd:\mp3\podcast\Alarm'
   # directoryMonth = '201701-test'
-  directoryMonth = '201701'
+  directoryMonth = '201704'
   directoryWork = directoryMaster + os.sep + directoryMonth
   directoryCurrent = os.path.dirname(os.path.realpath(__file__))
   weekdays = ['ponedeljak', 'utorak', 'sreda', 'cetvrtak', 'petak']
+  logger.debug('Config finished.')
+  
+  def configLoad(configName):
+    '''Loads configuration from configName  and returns as list configuration'''
+    pass
 
+  def configWrite(configList, configOutputFile=configFile)  :
+    '''Writes configuration; configList into configOutputFIle'''
+    pass
+  
+  def configApply(configList)  :
+    '''Applies configuration from configList'''
+    pass
 
   def readMp3Filenames(dirWorking):
     '''Scans given absolute path for mp3 files and returns them as list'''
+    logger.debug('readMp3Filenames() started')
     fileNamesAll = os.listdir(dirWorking)
     fileNamesMp3 = []
     for fileName in fileNamesAll:
@@ -30,17 +47,18 @@ def main():
         fileNamesMp3.append(fileName)
       else:
         logger.debug('{0} is not mp3'.format(fileName))
+    logger.debug('readMp3Filenames() finished')
     return fileNamesMp3
 
 
   def renameMp3Filenames(inputMp3Filenames, inputDirectory):
     '''Renames list of mp3 files in given directory, returns nothing.
-    {weekday}_{DD}.{MM}.{YYYY}1.mp3 -> {YYYY}.{MM}.{DD}_{weekday}.mp3
+    {weekday}_{DD}.{MM}.{YYYY}bm.mp3 -> {YYYY}.{MM}.{DD}_{weekday}.mp3
     '''
     # hardcoded, there will be trouble latter
-    # there can be problem if there is weekday somewhere else than at begging
+    # there can be problem if there is weekday somewhere else than at begging, but wrong filename format
     # TODO: test for relative path, works with absolute path only
-    logger.debug('input Filename, new Filename')
+    logger.debug('renameMp3Filenames() started.')
     for fileName in inputMp3Filenames:
       targetFilename = ''
       counterDays = 0
@@ -50,22 +68,22 @@ def main():
           logger.warn('{0} already renamed to target format.'.format(fileName))
         elif weekdayTest == 0:
           logger.debug('{0} proccessing started.'.format(fileName))
-          logger.debug(fileName, day)
+          logger.debug('filename: {0}  weekday: {1}'.format(fileName, day))
           fileNameTemp = fileName[(len(day) + 1):]
-          fileNameTempList = fileNameTemp.split('.')
-          # remove unneccessery figure at end YYYY string
-          if len(fileNameTempList[2]) > 4:
-            logger.debug('Additional figure present in year (YYYYx)')
-            fileNameTempList[2] = fileNameTempList[2][0:-1]
+          tempDD, tempMM, tempYYYY, tempExtension = fileNameTemp.split('.')
+          # add . at between YYYY and 'bm' string
+          if len(tempYYYY) > 4:
+            logger.debug('Additional string present in year: {0}'.format(tempYYYY))
+            tempYYYY, tempAdditional = tempYYYY[0:-2], tempYYYY[-2:]
           else:
-            logger.debug('Correct number of figures in year (YYYY)')
-          logger.debug('Rearanging timestamp in filename started.')
-          tempString = '_'.join([fileNameTempList[0], day])
-          fileNameTempList[0] = tempString
-          fileNameTempListOrder = [fileNameTempList[2], fileNameTempList[1],
-                                   fileNameTempList[0], fileNameTempList[3]]
+            logger.debug('Correct number of figures in year: {0}'.format(tempYYYY))
+          logger.debug('Rearanging timestamp in filename started: {0}.'.format(fileName))
+          tempString = '_'.join([tempDD, day])
+          tempDD = tempString
+          fileNameTempListOrder = [tempYYYY, tempMM, tempDD, tempAdditional, tempExtension]
           targetFilename = '.'.join(fileNameTempListOrder)
-          logger.debug('Rearanging timestamp in filename finished.')
+          logger.debug('Rearanging timestamp in filename finished: {0}.'.format(targetFilename))
+          
           if not os.path.isfile(inputDirectory + os.sep + targetFilename):
             logger.debug('OK to rename, target {0} not found.'.format(targetFilename))
             logger.debug(inputDirectory)
@@ -82,6 +100,7 @@ def main():
             logger.warn('weekday not found in {0}'.format(fileName))
         else:
           logger.error('TODO: unknown case for weekday in {0}'.format(fileName))
+    logger.debug('renameMp3Filenames() finished.')
   
   logging.info('Start')
   mp3Files = readMp3Filenames(directoryWork)
